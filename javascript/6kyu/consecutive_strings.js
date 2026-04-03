787cb5eb18708c64d99d74a00511aaa7b98d43b9
Davi Luna <kiernetwork@gmail.com>
2026-04-03 18:09:53 -0300

    More exercised solved

---
+++ javascript/6kyu/consecutive_strings.js
@@ -0,0 +1,14 @@
+function longestConsec(strarr, k) {
+  if (k <= 0 || k > strarr.length) return '';
+
+  let longest = '';
+
+  for (let i = 0; i <= strarr.length - k; i++) {
+    const current = strarr.slice(i, i + k).join('');
+    if (current.length > longest.length) {
+      longest = current;
+    }
+  }
+
+  return longest;
+}
